d26884d5d56990a770db74f7c4215e62e0d8626d
Yumeme <yume@sfc.wide.ad.jp>
2018-09-09 00:41:15 +0900

    first.c

---
+++ atcoder/first.c
@@ -0,0 +1,16 @@
+// om0ch1
+// first.c
+// A - はじめてのあっとこーだー（Welcome to AtCoder）
+// https://beta.atcoder.jp/contests/practice/tasks/practice_1
+
+# include <stdio.h>
+
+int main(){
+    int a, b, c;
+    char s[101];
+    scanf("%d", &a);
+    scanf("%d" "%d", &b, &c);
+    scanf("%s", s);
+    printf("%d %s\n", a+b+c, s);
+    return 0;
+}
